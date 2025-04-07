cdb9d2857e591f930a66a3fa805e87e15468a0b6
acode03 <khana3@carleton.edu>
2025-04-07 18:13:34 -0500

    Delete src directory

--- src/MinimaxNode.py
+++
@@ -1,7 +0,0 @@
-class MiniMaxNode:
-    def __init__(self) -> None:
-        self.is_max_player = False 
-        self.move = None
-        self.score = 0
-        self.children = []
-        self.choices = []
